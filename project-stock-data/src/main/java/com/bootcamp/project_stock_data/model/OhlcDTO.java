71ff3cd2025bd8ea5bc0da4d1b79abb603835a63
Anson.chan <orz17891@gmail.com>
2026-03-19 17:22:49 +0800

    update

--- project-stock-data/src/main/java/com/bootcamp/project_stock_data/model/OhlcDTO.java
+++ project-stock-data/src/main/java/com/bootcamp/project_stock_data/model/OhlcDTO.java
@@ -3,7 +3,6 @@ package com.bootcamp.project_stock_data.model;
 import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,7 +15,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 
 public class OhlcDTO {
-  @JsonProperty("tran_date")
+  // @JsonProperty("tran_date")
   @JsonFormat(pattern = "yyyy-MM-dd")
   private LocalDate tranDate;
   private String symbol;
